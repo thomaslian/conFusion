275cfbdc86c3dd00d6c2a8b19c9bd2dfef847676
Thomas Oedegaard <thomaslio@live.no>
2020-04-30 16:00:07 +0200

    Reactive forms part one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,11 @@ import { MatListModule } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from  '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 
@@ -49,6 +53,9 @@ import { LoginComponent } from './login/login.component';
     BrowserAnimationsModule,
     MatCheckboxModule,
     FormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    ReactiveFormsModule,
     MatInputModule,
     FlexLayoutModule,
     AppRoutingModule,
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Feedback, ContactType } from '../shared/Feedback';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  // Form group module that is hosting the reactive form
+  feedbackForm: FormGroup;
+  feedback: Feedback;
+  contactType = ContactType;
+
+  // Constructor purpose is to help prepare the creation of a new instance of the class
+  // Instance - A object in memory
+  constructor(private fb: FormBuilder) { 
+    //Call the createForm class to build the form
+    this.createForm();
+  }
 
   ngOnInit() {
   }
 
+  // When createForm() is called, it will create the reactive form 
+  createForm() {
+    //this.fb.group({}) - Allows us to define a FormGroup
+    this.feedbackForm = this.fb.group({
+      firstname: '',
+      lastname: '',
+      telnum: 0,
+      email: '',
+      agree: false,
+      contacttype: 'None',
+      message: ''
+    });
+  }
+
+  onSubmit(){
+    // Creates a javascript object from the form that can be passed into the Feedback class
+    this.feedback = this.feedbackForm.value;
+    console.log(this.feedback);
+    this.feedbackForm.reset();
+  }
+
 }
+
+
